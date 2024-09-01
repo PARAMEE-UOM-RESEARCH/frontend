911dc8bafcd908d6c0cf23e681f3be08b34d3fe3
SahanKumarasiri <kumarasirisahan@gmail.com>
2024-09-01 13:34:10 +0530

    Hotels API Resolved

--- src/components/DashboardSubComponents/Hotels.js
+++ src/components/DashboardSubComponents/Hotels.js
@@ -9,13 +9,19 @@ const Hotels = ({ location }) => {
 
   useEffect(() => {
     (async () => {
+      const today = new Date();
+      const arrivalDate = today.toISOString().split("T")[0];
+
+      const tomorrow = new Date(today);
+      tomorrow.setDate(today.getDate() + 1);
+      const departureDate = tomorrow.toISOString().split("T")[0];
       await axios
         .get(
           `${REACT_APP_BASE_URL}/hotels/searchByCoordinates?latitude=${
             location.latitude
           }&longitude=${
             location.longitude
-          }&arrival_date=2024-08-19&departure_date=2024-08-20&adults=${1}&children_age=${0}&room_qty=${1}&units=metric&page_number=1&temperature_unit=c&languagecode=en-us&currency_code=USD`
+          }&arrival_date=${arrivalDate}&departure_date=${departureDate}&adults=${1}&children_age=${0}&room_qty=${1}&units=metric&page_number=1&temperature_unit=c&languagecode=en-us&currency_code=USD`
         )
         .then((res) => setHotels(res?.data?.data?.result))
         .catch((err) => message(err));
